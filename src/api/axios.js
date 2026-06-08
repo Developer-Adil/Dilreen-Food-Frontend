2b50f8d464270bf32deba808aeccf076bfa160a9
Mohammad Adil Khan <adilkhankhan2203@gmail.com>
2026-06-08 21:01:38 +0530

    fix api base url with api prefix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,23 +1,20 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://dilreen-food.onrender.com/api",
+  baseURL: "https://dilreen-food.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-API.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+  return config;
+});
 
 export default API;
